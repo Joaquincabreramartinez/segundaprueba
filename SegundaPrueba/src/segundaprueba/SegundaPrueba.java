a7d81d8771d8744f7eb46c902e18908d2faa0bd0
Joaquin <Joaquin@DESKTOP-5FAD0MS>
2022-12-05 17:56:24 +0100

    tercer comit
    tercer comit

--- SegundaPrueba/src/segundaprueba/SegundaPrueba.java
+++ SegundaPrueba/src/segundaprueba/SegundaPrueba.java
@@ -5,6 +5,7 @@ public class SegundaPrueba {
 	public static void main(String[] args) {
 		System.out.println("primer commit");
 		System.out.println("segundo commit commit");
+		System.out.println("tercer commit commit");
 
 	}
 
